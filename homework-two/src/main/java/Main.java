7f32f03afbdb30a530ba600c440bd36589ef757c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-07-12 17:12:14 +0300

    add new method

--- homework-two/src/main/java/Main.java
+++ homework-two/src/main/java/Main.java
@@ -1,4 +1,4 @@
-import java.util.Objects;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,12 +8,17 @@ public class Main {
         System.out.println(isIsrPhoneNumber("71231234567"));
 
         printSubStringReverse("Hello world!", 1, 4);
+
+        System.out.println(getWordsReverse("Hello my nice world"));
+
+        printWordsReverseInColumn("Hello my nice world");
     }
 
     public static void printStringReverse(String string) {
         if (Objects.isNull(string) || string.isBlank()) {
             System.out.println("Wrong string");
         }
+
         System.out.println(new StringBuilder(string).reverse());
     }
 
@@ -21,8 +26,8 @@ public class Main {
         if (Objects.isNull(phone) || phone.isBlank()) {
             return null;
         }
-        return phone.matches("7\\d{10}");
 
+        return phone.matches("7\\d{10}");
     }
 
     public static void printSubStringReverse(String string, int start, int finish) {
@@ -38,5 +43,24 @@ public class Main {
         String srt = string.substring(0, start);
         String str2 = string.substring(finish + 1);
         System.out.println(srt + builder.reverse() + str2);
+    }
+
+    public static String getWordsReverse(String string) {
+        if (Objects.isNull(string) || string.isBlank()) {
+            return null;
+        }
+
+        return String.join(" ", Arrays.asList(string.split(" ")).reversed());
+    }
+
+    public static void printWordsReverseInColumn(String string) {
+        if (Objects.isNull(string) || string.isBlank()) {
+            System.out.println("Wrong string");
+        }
+
+        Arrays.stream(string.split(" "))
+                .map(StringBuilder::new)
+                .peek(StringBuilder::reverse)
+                .forEach(System.out::println);
     }
 }
