cebdd75ca37d8d39c3a28c9eef26dbf635647a4f
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-07-16 15:07:47 +0300

    use lombok from class Book

---
+++ homework-stream/src/main/java/Book.java
@@ -0,0 +1,28 @@
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Getter
+public class Book {
+    @Setter
+    private String author;
+    @Setter
+    private String title;
+    @Setter
+    private double price;
+    private List<String> reviews;
+
+    public Book(String author, String title, double price) {
+        this.author = author;
+        this.title = title;
+        this.price = price;
+        this.reviews = new ArrayList<>();
+    }
+
+    public void addReview(String review) {
+        reviews.add(review);
+    }
+}
