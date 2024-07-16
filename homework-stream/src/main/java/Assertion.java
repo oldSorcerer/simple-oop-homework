fdccef4d3d816a8a197dbd39643ac031d89f5962
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-07-16 15:07:04 +0300

    format text

--- homework-stream/src/main/java/Assertion.java
+++ homework-stream/src/main/java/Assertion.java
@@ -2,14 +2,14 @@ public class Assertion {
 
     private static int numberOfTask = 1;
 
-    static void assertEquals(Object expected, Object actual) throws IllegalArgumentException {
+    public static void assertEquals(Object expected, Object actual) throws IllegalArgumentException {
         if (!expected.equals(actual)) {
             throw new IllegalArgumentException(
                     String.format("expected result is %s , but actual %s",
                             expected,
                             actual));
         } else {
-            System.out.println(String.format("task %s - done", numberOfTask++));
+            System.out.printf("task %s - done%n", numberOfTask++);
         }
     }
 }
